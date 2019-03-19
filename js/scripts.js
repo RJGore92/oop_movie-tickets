ae64dfec1904a2166f84c5e4a38b010142812455
Robert Gore <DarkburstGX@gmail.com>
2019-03-19 13:44:49 -0700

    Intermission commit for Movie Tickets app, updates JavaScript and HTML, and creates minor styling for the form jumbotron in CSS.

--- js/scripts.js
+++ js/scripts.js
@@ -38,6 +38,7 @@ TicketList.prototype.deleteTicket = function(ticketID) {
 
 var currentTicketList = new TicketList();
 
+
 function MovieList() {
   this.availableMovies = [],
   this.availableMovieCount = 0
@@ -76,14 +77,107 @@ MovieList.prototype.removeMovie = function (movieNumber) {
   return false;
 };
 
+MovieList.prototype.appendToList = function () {
+  var movieListToAppend = this.availableMovies;
+  var movieIDFocus = 0;
+  movieListToAppend.forEach(function(movie) {
+    movieIDFocus += 1;
+    $("select#movie-selector").append("<option value='"+movieIDFocus+"'>"+movie.name+" ("+movie.rating+")");
+  });
+};
+
+var currentMovieList = new MovieList();
 
 function Movie(name, releaseYear, rating) {
   this.name = name,
   this.releaseYear = releaseYear,
   this.rating = rating
-
 }
 
-//MovieList.prototype.determinePriceFactor  = function () {
-//
-//};
+Movie.prototype.ratingSafeAge = function () {
+  var ratingAgeCheck = this.rating;
+  var ratingToLower = ratingAgeCheck.toLowerCase();
+  var minAge = 0;
+  if (ratingAgeCheck == "g") {
+    return minAge;
+  }
+  else if (ratingAgeCheck == "pg") {
+    minAge = 7;
+    return minAge;
+  }
+  else if (ratingAgeCheck == "pg-13") {
+    minAge = 13;
+    return minAge;
+  }
+  else {
+    minAge = 21;
+    return minAge;
+  }
+};
+
+var movieOne = new Movie("TestMovieA", 2015, "G");
+var movieTwo = new Movie("TestMovieB", 2012, "PG-13");
+var movieThree = new Movie("TestMovieC", 2019, "R");
+
+function Ticket(selectedMovie, viewerAge) {
+  this.selectedMovie = selectedMovie,
+  this.viewerAge = viewerAge
+  //this.movieTime = movieTime
+}
+
+Ticket.prototype.determineSafeToView  = function () {
+  var ageCheck = this.viewerAge;
+  var warningRating = 0;
+  if (this.selectedMovie) {
+    var movieAgeCheck = this.selectedMovie.ratingSafeAge();
+    if (movieAgeCheck > ageCheck) {
+      alert("The movie you bought a ticket for doesn't appear to be safe for your age, even with supervision. We suggest that you look into a different movie.");
+      warningRating = 2;
+    }
+    else if (movieAgeCheck == ageCheck) {
+      alert("This movie appears to be barely suitable for your age.  Be aware that some subjects of this movie be inappropriate for you.");
+      warningRating = 1;
+    }
+    else {
+      alert("This movie is suitable for your age.  Grab your snacks, then sit back and enjoy the show.");
+    }
+  }
+  return warningRating;
+};
+
+function confirmPurchase(ticket) {
+  var pendingTicketID = currentTicketList.currentTicket + 1;
+  var movieToTarget = ticket.selectedMovie;
+  var checkWarning = ticket.determineSafeToView();
+  if (checkWarning == 2) {
+    return false;
+  }
+  else if (checkWarning == 1) {
+    confirm("Are you sure that you wish to view this movie?");
+  }
+  else {
+    return true;
+  }
+}
+
+$(document).ready(function(){
+  currentMovieList.addMovieToList(movieOne);
+  currentMovieList.addMovieToList(movieTwo);
+  currentMovieList.addMovieToList(movieThree);
+  currentMovieList.appendToList();
+  $("form#ticket-giver").submit(function(event) {
+    event.preventDefault();
+    var movieSelected = currentMovieList.findMovie($("select#movie-selector").val());
+    console.log(movieSelected);
+    var ageOfAttendee = $("input#viewer-age-input").val();
+    var pendingTicket = new Ticket(movieSelected, ageOfAttendee);
+    var purchaseConfirmation = confirmPurchase(pendingTicket);
+    $("input#viewer-age-input").val("");
+    if (purchaseConfirmation) {
+      currentTicketList.giveTicket(pendingTicket);
+    }
+    else {
+      return false;
+    }
+  });
+});
